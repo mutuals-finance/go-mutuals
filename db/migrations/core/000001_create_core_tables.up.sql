f97872345013513b40bd6710eb22defb50e9ef6b
Fabian Piper <fabianpiper@web.de>
2023-08-14 13:13:38 +0200

    refactor: update db schema to include only used tables

--- db/migrations/core/000001_create_core_tables.up.sql
+++ db/migrations/core/000001_create_core_tables.up.sql
@@ -43,25 +43,6 @@ CREATE INDEX users_fts_username_idx ON users USING gin (fts_username);
 
 CREATE INDEX users_wallets_idx ON users USING gin (wallets) WHERE (deleted = false);
 
-CREATE TABLE IF NOT EXISTS collections
-(
-    id              character varying(255) PRIMARY KEY NOT NULL,
-    deleted         boolean                            NOT NULL DEFAULT false,
-    owner_user_id   character varying(255),
-    nfts            character varying(255)[],
-    version         integer                                     DEFAULT 0,
-    last_updated    timestamp with time zone           NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    created_at      timestamp with time zone           NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    hidden          boolean                            NOT NULL DEFAULT false,
-    collectors_note character varying,
-    name            character varying(255),
-    layout          jsonb,
-    token_settings  jsonb,
-    split_id        character varying                  NOT NULL
-);
-
-CREATE INDEX collections_owner_idx ON collections USING btree (owner_user_id) WHERE (deleted = false);
-
 CREATE TABLE IF NOT EXISTS splits
 (
     id                      character varying(255) PRIMARY KEY NOT NULL,
@@ -69,59 +50,24 @@ CREATE TABLE IF NOT EXISTS splits
     last_updated            timestamp with time zone           NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at              timestamp with time zone           NOT NULL DEFAULT CURRENT_TIMESTAMP,
     version                 integer                                     DEFAULT 0,
-    owner_user_id           character varying(255),
-    collections             character varying(255)[],
     name                    character varying                  NOT NULL DEFAULT ''::character varying,
     description             character varying                  NOT NULL DEFAULT ''::character varying,
+    address                 character varying(255),
+    chain                   integer,
     hidden                  boolean                            NOT NULL DEFAULT false,
-    position                character varying                  NOT NULL,
+    badge_url               character varying,
     fts_name                tsvector GENERATED ALWAYS AS (to_tsvector('simple'::regconfig, (name)::text)) STORED,
-    fts_description_english tsvector GENERATED ALWAYS AS (to_tsvector('english'::regconfig, (description)::text)) STORED
+    fts_description_english tsvector GENERATED ALWAYS AS (to_tsvector('english'::regconfig, (description)::text)) STORED,
+--     fts_address             tsvector GENERATED ALWAYS AS (to_tsvector('simple'::regconfig, (name)::text)) STORED
 );
 
 CREATE INDEX splits_fts_description_english_idx ON splits USING gin (fts_description_english);
 
 CREATE INDEX splits_fts_name_idx ON splits USING gin (fts_name);
 
-ALTER TABLE collections
-    ADD CONSTRAINT collections_split_id_fkey
-        FOREIGN KEY (split_id) REFERENCES splits (id);
+-- CREATE INDEX splits_fts_address_idx ON splits USING gin (fts_address);
 
-CREATE TABLE IF NOT EXISTS contracts
-(
-    id                      character varying(255) PRIMARY KEY NOT NULL,
-    deleted                 boolean                            NOT NULL DEFAULT false,
-    version                 integer                                     DEFAULT 0,
-    created_at              timestamp with time zone           NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    last_updated            timestamp with time zone           NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    name                    character varying,
-    symbol                  character varying,
-    address                 character varying(255),
-    creator_address         character varying(255),
-    chain                   integer,
-    profile_banner_url      character varying,
-    profile_image_url       character varying,
-    badge_url               character varying,
-    description             character varying,
-    fts_address             tsvector GENERATED ALWAYS AS (setweight(
-            to_tsvector('simple'::regconfig, (COALESCE(address, ''::character varying))::text), (
-                CASE
-                    WHEN (chain <> 5) THEN 'A'::text
-                    ELSE 'D'::text
-                    END)::"char")) STORED,
-    fts_name                tsvector GENERATED ALWAYS AS (to_tsvector('simple'::regconfig,
-                                                                      (COALESCE(name, ''::character varying))::text)) STORED,
-    fts_description_english tsvector GENERATED ALWAYS AS (to_tsvector('english'::regconfig,
-                                                                      (COALESCE(description, ''::character varying))::text)) STORED
-);
-
-CREATE UNIQUE INDEX contract_address_chain_idx ON contracts USING btree (address, chain);
-
-CREATE INDEX contracts_fts_address_idx ON contracts USING gin (fts_address);
-
-CREATE INDEX contracts_fts_description_english_idx ON contracts USING gin (fts_description_english);
-
-CREATE INDEX contracts_fts_name_idx ON contracts USING gin (fts_name);
+CREATE UNIQUE INDEX split_address_chain_idx ON splits USING btree (address, chain);
 
 CREATE TABLE IF NOT EXISTS tokens
 (
@@ -145,24 +91,20 @@ CREATE TABLE IF NOT EXISTS tokens
     owner_user_id           character varying(255),
     owned_by_wallets        character varying(255)[]           NOT NULL,
     chain                   integer,
-    contract                character varying(255),
+    address                 character varying(255),
     is_user_marked_spam     boolean,
     is_provider_marked_spam boolean,
     last_synced             timestamp with time zone           NOT NULL DEFAULT now()
 );
 
+-- TODO add relation token -> split?
+
 CREATE INDEX block_number_idx ON tokens USING btree (block_number);
 
-CREATE INDEX owner_user_id_idx ON tokens USING btree (owner_user_id);
+CREATE INDEX token_id_address_chain_idx ON tokens USING btree (token_id, address, chain);
 
-CREATE INDEX token_id_contract_chain_idx ON tokens USING btree (token_id, contract, chain);
-
-CREATE UNIQUE INDEX token_id_contract_chain_owner_user_id_idx ON tokens USING btree (token_id, contract, chain, owner_user_id) WHERE (deleted = false);
-
-CREATE INDEX token_owned_by_wallets_idx ON tokens USING gin (owned_by_wallets);
-
-CREATE INDEX tokens_contract_owner_user_id_idx ON tokens USING btree (contract, owner_user_id) WHERE (deleted = false);
-
+/*
+TODO create relevance for split ie according to volume
 CREATE MATERIALIZED VIEW contract_relevance AS
 WITH users_per_contract AS (SELECT con.id
                             FROM contracts con,
@@ -194,6 +136,7 @@ FROM min_count,
      max_count;
 
 CREATE UNIQUE INDEX contract_relevance_id_idx ON contract_relevance USING btree (id);
+*/
 
 CREATE TABLE IF NOT EXISTS dev_metadata_users
 (
@@ -215,14 +158,12 @@ CREATE TABLE IF NOT EXISTS events
     subject_id       character varying(255)             NOT NULL,
     user_id          character varying(255),
     token_id         character varying(255),
-    collection_id    character varying(255),
     action           character varying(255)             NOT NULL,
     data             jsonb,
     deleted          boolean                            NOT NULL DEFAULT false,
     last_updated     timestamp with time zone           NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at       timestamp with time zone           NOT NULL DEFAULT CURRENT_TIMESTAMP,
     split_id         character varying(255),
-    comment_id       character varying(255),
     external_id      character varying(255),
     caption          character varying,
     group_id         character varying(255)
@@ -231,19 +172,13 @@ CREATE TABLE IF NOT EXISTS events
 CREATE INDEX events_actor_id_action_created_at_idx ON events USING btree (actor_id, action, created_at);
 
 CREATE INDEX events_split_edit_idx ON events USING btree (created_at, actor_id) WHERE ((action)::text = ANY
-                                                                                       ((ARRAY ['CollectionCreated'::character varying, 'CollectorsNoteAddedToCollection'::character varying, 'CollectorsNoteAddedToToken'::character varying, 'TokensAddedToCollection'::character varying, 'SplitInfoUpdated'::character varying])::text[]));
-
-CREATE INDEX events_visits_created_at_idx ON events USING btree (created_at) WHERE ((action)::text = 'ViewedSplit'::text);
+                                                                                       ((ARRAY ['SplitCreated'::character varying, 'TokensAddedToSplit'::character varying, 'SplitInfoUpdated'::character varying])::text[]));
 
 CREATE INDEX group_id_idx ON events USING btree (group_id);
 
 ALTER TABLE events
     ADD CONSTRAINT events_actor_id_fkey
         FOREIGN KEY (actor_id) REFERENCES users (id);
-
-ALTER TABLE events
-    ADD CONSTRAINT events_collection_id_fkey
-        FOREIGN KEY (collection_id) REFERENCES collections (id);
 
 ALTER TABLE events
     ADD CONSTRAINT events_split_id_fkey
@@ -269,15 +204,6 @@ CREATE TABLE IF NOT EXISTS legacy_views
 ALTER TABLE legacy_views
     ADD CONSTRAINT legacy_views_user_id_fkey
         FOREIGN KEY (user_id) REFERENCES users (id);
-
-CREATE TABLE IF NOT EXISTS marketplace_contracts
-(
-    contract_id character varying(255) PRIMARY KEY NOT NULL
-);
-
-ALTER TABLE marketplace_contracts
-    ADD CONSTRAINT marketplace_contracts_contract_id_fkey
-        FOREIGN KEY (contract_id) REFERENCES contracts (id);
 
 CREATE TABLE IF NOT EXISTS nonces
 (
@@ -316,90 +242,9 @@ ALTER TABLE notifications
     ADD CONSTRAINT notifications_split_id_fkey
         FOREIGN KEY (split_id) REFERENCES splits (id);
 
-CREATE MATERIALIZED VIEW owned_contracts AS
-WITH owned_contracts AS (SELECT users.id         AS user_id,
-                                users.created_at AS user_created_at,
-                                contracts.id     AS contract_id,
-                                count(tokens.id) AS owned_count
-                         FROM ((users
-                             JOIN tokens
-                                ON (((tokens.deleted = false) AND ((users.id)::text = (tokens.owner_user_id)::text) AND
-                                     (COALESCE(tokens.is_user_marked_spam, false) = false))))
-                             JOIN contracts
-                               ON (((contracts.deleted = false) AND ((tokens.contract)::text = (contracts.id)::text))))
-                         WHERE ((users.deleted = false) AND (users.universal = false))
-                         GROUP BY users.id, contracts.id),
-     displayed_tokens AS (SELECT owned_contracts_1.user_id,
-                                 owned_contracts_1.contract_id,
-                                 tokens.id AS token_id
-                          FROM owned_contracts owned_contracts_1,
-                               splits,
-                               collections,
-                               tokens
-                          WHERE ((splits.deleted = false) AND (collections.deleted = false) AND
-                                 ((splits.owner_user_id)::text = (owned_contracts_1.user_id)::text) AND
-                                 ((collections.owner_user_id)::text = (owned_contracts_1.user_id)::text) AND
-                                 ((tokens.owner_user_id)::text = (owned_contracts_1.user_id)::text) AND
-                                 ((tokens.contract)::text = (owned_contracts_1.contract_id)::text) AND
-                                 ((tokens.id)::text = ANY ((collections.nfts)::text[])))
-                          GROUP BY owned_contracts_1.user_id, owned_contracts_1.contract_id, tokens.id),
-     displayed_contracts AS (SELECT displayed_tokens.user_id,
-                                    displayed_tokens.contract_id,
-                                    count(displayed_tokens.token_id) AS displayed_count
-                             FROM displayed_tokens
-                             GROUP BY displayed_tokens.user_id, displayed_tokens.contract_id)
-SELECT owned_contracts.user_id,
-       owned_contracts.user_created_at,
-       owned_contracts.contract_id,
-       owned_contracts.owned_count,
-       COALESCE(displayed_contracts.displayed_count, (0)::bigint) AS displayed_count,
-       (displayed_contracts.displayed_count IS NOT NULL)          AS displayed,
-       now()                                                      AS last_updated
-FROM (owned_contracts
-    LEFT JOIN displayed_contracts ON ((((displayed_contracts.user_id)::text = (owned_contracts.user_id)::text) AND
-                                       ((displayed_contracts.contract_id)::text =
-                                        (owned_contracts.contract_id)::text))));
-
-CREATE UNIQUE INDEX owned_contracts_user_contract_idx ON owned_contracts USING btree (user_id, contract_id);
-
-CREATE INDEX owned_contracts_user_created_at_idx ON owned_contracts USING btree (user_created_at);
-
-CREATE INDEX owned_contracts_user_displayed_idx ON owned_contracts USING btree (user_id, displayed);
-
-CREATE TABLE IF NOT EXISTS recommendation_results
-(
-    id                  character varying(255) PRIMARY KEY NOT NULL,
-    version             integer                                     DEFAULT 0,
-    user_id             character varying(255),
-    recommended_user_id character varying(255),
-    recommended_count   integer,
-    created_at          timestamp with time zone           NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    last_updated        timestamp with time zone           NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    deleted             boolean                            NOT NULL DEFAULT false
-);
-
-ALTER TABLE recommendation_results
-    ADD CONSTRAINT recommendation_results_user_id_recommended_user_id_version_key
-        UNIQUE (user_id, recommended_user_id, version);
-
-ALTER TABLE recommendation_results
-    ADD CONSTRAINT user_id_not_equal_recommend_id_constraint
-        CHECK (((user_id)::text <> (recommended_user_id)::text));
-
-ALTER TABLE recommendation_results
-    ADD CONSTRAINT recommendation_results_recommended_user_id_fkey
-        FOREIGN KEY (recommended_user_id) REFERENCES users (id);
-
-ALTER TABLE recommendation_results
-    ADD CONSTRAINT recommendation_results_user_id_fkey
-        FOREIGN KEY (user_id) REFERENCES users (id);
-
-CREATE TABLE IF NOT EXISTS schema_migrations
-(
-    version bigint PRIMARY KEY NOT NULL,
-    dirty   boolean            NOT NULL
-);
-
+-- Spam scores for newly-created users. Contains all newly created users,
+-- but users with score 0 can typically be ignored since they're not likely to
+-- be spam.
 CREATE TABLE IF NOT EXISTS spam_user_scores
 (
     user_id         character varying(255) PRIMARY KEY NOT NULL,
@@ -416,6 +261,8 @@ ALTER TABLE spam_user_scores
     ADD CONSTRAINT spam_user_scores_user_id_fkey
         FOREIGN KEY (user_id) REFERENCES users (id);
 
+/*
+TODO see above -> split_relevance depending on volume better?
 CREATE MATERIALIZED VIEW split_relevance AS
 WITH views_per_split AS (SELECT events.split_id,
                                 count(events.split_id) AS count
@@ -439,46 +286,7 @@ FROM min_count,
      max_count;
 
 CREATE UNIQUE INDEX split_relevance_id_idx ON split_relevance USING btree (id);
-
-CREATE MATERIALIZED VIEW top_recommended_users AS
-SELECT recommendation_results.recommended_user_id,
-       count(DISTINCT recommendation_results.user_id) AS frequency,
-       now()                                          AS last_updated
-FROM recommendation_results
-WHERE ((recommendation_results.version = 0) AND (recommendation_results.deleted = false) AND
-       (recommendation_results.last_updated >= (now() - '30 days'::interval)))
-GROUP BY recommendation_results.recommended_user_id
-ORDER BY (count(DISTINCT recommendation_results.user_id)) DESC, (now()) DESC
-LIMIT 100;
-
-CREATE UNIQUE INDEX top_recommended_users_pk_idx ON top_recommended_users USING btree (recommended_user_id);
-
-CREATE MATERIALIZED VIEW user_relevance AS
-WITH followers_per_user AS (SELECT users.id,
-                                   count(follows.*) AS count
-                            FROM users,
-                                 follows
-                            WHERE (((follows.followee)::text = (users.id)::text) AND (users.deleted = false) AND
-                                   (follows.deleted = false))
-                            GROUP BY users.id),
-     min_count AS (SELECT 1 AS count),
-     max_count AS (SELECT followers_per_user.count
-                   FROM followers_per_user
-                   ORDER BY followers_per_user.count DESC
-                   LIMIT 1)
-SELECT followers_per_user.id,
-       (((min_count.count + followers_per_user.count))::numeric /
-        ((min_count.count + max_count.count))::numeric) AS score
-FROM followers_per_user,
-     min_count,
-     max_count
-UNION
-SELECT NULL::character varying                                                       AS id,
-       ((min_count.count)::numeric / ((min_count.count + max_count.count))::numeric) AS score
-FROM min_count,
-     max_count;
-
-CREATE UNIQUE INDEX user_relevance_id_idx ON user_relevance USING btree (id);
+*/
 
 CREATE TABLE IF NOT EXISTS user_roles
 (
