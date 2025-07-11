4869da26b46e3ab5d35bf76cc0bbb7ec1ae48f37
Fabian Piper <fabianpiper@web.de>
2025-07-11 19:20:30 +0200

    fix: remove indexer dir

--- indexer/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .account import Account
-from .base_extension import BaseExtension
-from .pool import Pool
-from .pool_factory import PoolFactory
-from .registry import Registry
-from .token import Token
-from .token_balance import TokenBalance
-
-__all__ = ['Account', 'BaseExtension', 'Pool', 'PoolFactory', 'Registry', 'Token', 'TokenBalance']
